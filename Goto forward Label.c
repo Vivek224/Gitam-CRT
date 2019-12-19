11090446d2d4bda6bb2d77a59d662e6a1d8c1840
Vivek <58965068+Vivek224@users.noreply.github.com>
2019-12-19 11:57:36 +0530

    Add files via upload

---
+++ Goto forward Label.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("\nGITAM UNIVERSITY\n");
+	goto a;
+	printf("\nDayanahitha Proffessional of studies\n");
+	printf("\nPromizeit Services Pvt Ltd\n");
+	a:
+	printf("\nHyderabad\n");
+	return 0;
+}
