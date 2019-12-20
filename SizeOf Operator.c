03e1870987ff930858e4f33315baa05fc42c152b
Vivek <58965068+Vivek224@users.noreply.github.com>
2019-12-20 09:55:07 +0530

    Add files via upload

---
+++ SizeOf Operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	char b='a';
+	float c=12.89;
+	printf("\nSize of(a)= %d",sizeof(a));
+	printf("\nSize of(b)= %d",sizeof(b));
+	printf("\nSize of(c)= %d",sizeof(c));
+	printf("\nSize of(int)= %d",sizeof(int));
+	printf("\nSize of(char)= %d",sizeof(char));
+	printf("\nSize of(float)= %d",sizeof(float));
+return 0;
+}
