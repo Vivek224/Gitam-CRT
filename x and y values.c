8b3f3ef51e26937e768f3c54da2034f13a946cbb
Vivek <58965068+Vivek224@users.noreply.github.com>
2019-12-20 09:12:56 +0530

    Update x and y values.c

--- x and y values.c
+++ x and y values.c
@@ -3,8 +3,8 @@ int main()
 {
 	int x=100,y=200;
 	if((x-=100)&&(y-=100))
-		printf("x=%d and y=%d",x,y);
+		printf("x= %d and y= %d",x,y);
 	else
-		printf("x=%d and y=%d",x,y);
+		printf("x= %d and y= %d",x,y);
 	return 0;
 }
