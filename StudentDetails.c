ca7efcb2dd2558ecb3fe4542645c5a02fa6332ff
Vivek <58965068+Vivek224@users.noreply.github.com>
2020-01-23 15:10:11 +0530

    Update StudentDetails.c

--- StudentDetails.c
+++ StudentDetails.c
@@ -3,6 +3,7 @@ struct student
 {
     int rollno,s1,s2,s3,total;
     char name[10];
+    float average;
 }s[10];
 void main()
 {
@@ -18,10 +19,11 @@ void main()
         printf("\nEnter the marks in 3 subjects : ");
         scanf("%d%d%d",&s[i].s1,&s[i].s2,&s[i].s3);
         s[i].total=s[i].s1+s[i].s2+s[i].s3;
+        s[i].average=s[i].total/3.0;
     }
-    printf("\nRoll No.Name\tSub1\tSub2\tSub3\tTotal\n\n");
+    printf("\nRoll No.Name\tSub1\tSub2\tSub3\tTotal\tAverage\n\n");
     for(i=0;i<n;i++)
     {
-        printf("%d\t%s\t%d\t%d\t%d\t%d\n",s[i].rollno,s[i].name,s[i].s1,s[i].s2,s[i].s3,s[i].total);
+        printf("%d\t%s\t%d\t%d\t%d\t%d\t%2f\n",s[i].rollno,s[i].name,s[i].s1,s[i].s2,s[i].s3,s[i].total,s[i].average);
     }
 }
