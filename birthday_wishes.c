4f03aa346d48ac8bebcf86042dfc543b2fecd684
Vivek <58965068+Vivek224@users.noreply.github.com>
2020-01-31 20:06:36 +0530

    Create birthday_wishes.c

---
+++ birthday_wishes.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+char  ch;
+    printf("\033[1;36m");
+    printf("  Hi there **PAVANI** : ) \n ");
+    printf("\033[0m");
+    printf(" \n ");
+    printf(" Wait be 'patient' :P .... \n");
+    printf(" \n ");
+    sleep(3);
+    printf(" Note :- first please read every thing you see .... \n ");
+    printf(" \n ");
+	printf(" -> Do you want to see this....? \n ");
+	printf(" \n ");
+	printf(" -> If 'yes' enter 'y' (y without quotations) and click enter          button in your keyboard. \n ");
+	printf(" \n ");
+	printf(" -> After entering the response , to see the whole screen don't        forget to click back button (only once) to hide the keyboard. \n ");
+	printf(" \n ");
+	printf(" -> Until you see the words '[Program finished]' don't look away       from the screen.... :) \n " );
+	printf(" \n ");
+	sleep(2);
+	printf("\033[1;32m");
+	printf(" Ok waiting here .... give your response :   ");
+	scanf("%c", &ch);
+	printf(" \n ");
+	printf("\033[0m");
+ 
+ if ( ch == 'y')
+ {
+ 	      printf(" \n ");
+ 	      printf(" Here goes my attempt to make you happy .... \n ");
+ 	      sleep(2);
+ 	      printf(" \n ");
+ 	      printf(" -> Dont forget #click back button now to hide the 'keyboard'          now \n ");
+ 	      sleep(2);
+ 	      printf(" \n ");
+ 	      sleep(1);
+ 	      printf("Ok , in... \n ");
+ 	      sleep(1);
+ 	      printf(" \n ");
+ 	      
+ 	      printf(" 3 \n ");
+ 	      
+ 	      printf(" \n ");
+ 	      sleep(1);
+ 	      
+ 	      printf(" 2 \n ");
+ 	      
+ 	      printf(" \n ");
+ 	      sleep(1);
+ 	      
+ 	      printf(" 1 \n ");
+ 	      printf(" \n ");
+ 	      printf(" Lights on.... 'DUDE' see down \n ");
+ 	      printf(" \n ");
+ 	      sleep(3);
+ 	      printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  \n");
+ 	      printf(" \n ");
+ 	      printf("\033[1;36m");
+ 	      
+ 	      
+ 	      printf("n   n    n     nnnnn  nnnnn  n     n \n ");
+           printf("n   n   n  n   n   n  n   n   n   n \n");
+ 	      printf(" nnnnn   nnnn   nnnnn  nnnnn    n n \n");
+           printf(" n   n   n  n   n      n         n \n");
+ 	      printf(" n   n   n  n   n      n         n \n");
+ 	      sleep(3);
+ 	      printf("\033[0m");
+ 	      printf(" \n ");
+ 	      printf("\033[1;36m ");
+ 	      printf(" \n ");
+ 	      printf("nnnn    nnnn  nnnn  nnnnn  n  n   nnnn    n    n     n \n");
+ 	      printf(" n   n    n    n  n    n    n  n   n   n  n  n   n   n \n");
+ 	      printf(" nnnn     n    nnnn    n    nnnn   n   n  nnnn    n n \n");
+ 	      printf(" n   n    n    n  n    n    n  n   n   n  n  n     n \n");
+ 	      printf(" nnnn    nnnn  n   n   n    n  n   nnnn   n  n     n  \n");
+ 	      sleep(2);
+ 	      printf(" \n ");
+ 	      sleep(2);
+ 	      printf(" \n ");
+ 	      sleep(3);
+ 	      printf("\033[0m");
+ 	      printf(" \n ");
+ 	       printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  \n");
+ 	       printf(" \n ");
+ 	      sleep(1);
+ 	      printf(" Hope it made you happy  my dear.... \n ");
+ 	      printf(" \n ");
+ 	      sleep(2);
+ 	      printf(" And I know it's '**pavani**' but i love calling you '**dhed dhana**' .... \n ");
+ 	      printf(" \n ");
+ 	      printf(" \n ");
+ 	      printf(" \n ");
+ 	      sleep(2);
+ 	      printf("                                         as ur bro.... *vivek* ");
+ 	      sleep(2);
+ 	      printf("                                                                                                                               'your' **name** : ) ");
+ 	      printf(" \n ");
+ 	      printf(" \n ");
+ 	      printf(" \n ");
+ 	      printf(" \n ");
+ 	      sleep(2);
+ 	      printf(" Keep smiling .... ALWAYS.... ");
+ 	      printf(" \n ");
+ 	      printf(" \n ");
+ 	      printf(" \n ");
+ 	      printf(" \n ");
+ }
+ else
+ {
+ printf(" \n ");
+ printf(" \n ");
+ printf(" \n ");
+ printf("  please enter \n ");
+ printf("\033[1;36m ");
+ printf(" yes (y) \n ");
+ printf("\033[0m");
+ printf("  to see the content.... \n ");
+ printf(" \n ");
+ printf("  (°_°) utterly disappointed ##run again \n ");
+ printf(" \n ");
+ printf(" \n ");
+ printf(" \n ");
+ }
+ return 0;
+}
